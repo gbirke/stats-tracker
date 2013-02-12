f6b596ef0301bf7bc2e622f743f51751ebc48f82
Gabriel Birke <birke@d-scribe.de>
2013-02-12 22:46:02 +0100

    Added Handlebars templates

--- app/main.js
+++ app/main.js
@@ -6,8 +6,14 @@ require.config({
         'backbone': '../vendors/backbone/backbone',
         'backbone.babysitter': '../vendors/backbone.babysitter/lib/amd/backbone.babysitter',
         'backbone.wreqr': '../vendors/backbone.wreqr/lib/amd/backbone.wreqr',
-        'marionette': '../vendors/backbone.marionette/lib/core/amd/backbone.marionette'
+        'backbone.marionette': '../vendors/backbone.marionette/lib/core/amd/backbone.marionette',
+        'backbone.marionette.handlebars': '../vendors/backbone.marionette.handlebars/src/backbone.marionette.handlebars',
+        'handlebars': '../vendors/handlebars/handlebars',
+        'json2': '../vendors/require-handlebars-plugin/hbs/json2',
+        'i18nprecompile': '../vendors/require-handlebars-plugin/hbs/i18nprecompile',
+        'hbs': '../vendors/require-handlebars-plugin/hbs'
     },
+    deps: [ "backbone.marionette.handlebars" ],
     shim: {
         'underscore': {
             exports: '_'
@@ -15,11 +21,15 @@ require.config({
         'backbone': {
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
+        },
+        'handlebars': {
+            exports: 'Handlebars'
         }
-    }
+    },
+    hbs: { disableI18n: true }
 });
 
-require(['jquery', 'marionette', 'views/app'], function ($, Marionette, AppView) {
+require(['jquery', 'backbone.marionette', 'views/app'], function ($, Marionette, AppView) {
 
     var statList = new Marionette.Region({el:'#statlist'}),
         v = new AppView();
--- app/views/app.js
+++ app/views/app.js
@@ -1,6 +1,9 @@
-define(['marionette'], function(Marionette) {
+define(['backbone.marionette', 'hbs!templates/stat-item'], function(Marionette, statItemTpl) {
   var App = Marionette.ItemView.extend({
-    template: "#statItem"
+    template: {
+    	type: "handlebars",
+    	template: statItemTpl
+    }
   });
 
   return App;
